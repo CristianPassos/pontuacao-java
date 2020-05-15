0fe83a03bf047dc95c623d6ac7a741ecc0295e08
Cristian Passos Silva <cristian.passos.silva@everis.com>
2020-05-15 19:48:58 -0300

    Leitura do volume

--- src/pontuacao/Programa.java
+++ src/pontuacao/Programa.java
@@ -7,10 +7,22 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		int qtdCompras, score;
+		
+		double ticketMedio;
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE ");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		System.out.printf("Quantas compras o cliente fez no último ano? ");
+		qtdCompras = sc.nextInt();
+		
+		System.out.printf("Qual o ticket médio? ");
+		ticketMedio = sc.nextDouble();
 	}
 
 }
