f7186ad45aee8b5ccb86c40ddade5596339c1f07
Cristian Passos Silva <cristian.passos.silva@everis.com>
2020-05-15 19:45:54 -0300

    Cabecalho

---
+++ src/pontuacao/Programa.java
@@ -0,0 +1,16 @@
+package pontuacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE ");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+	}
+
+}
