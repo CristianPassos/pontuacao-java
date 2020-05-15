304fd5ee093550bafa91f3c5aa6c92fce31fea0f
Cristian Passos Silva <cristian.passos.silva@everis.com>
2020-05-15 20:40:26 -0300

    Score de volume de compras

--- src/pontuacao/Programa.java
+++ src/pontuacao/Programa.java
@@ -14,23 +14,39 @@ public class Programa {
 		
 		char tipoPagamento;
 		
-		double ticketMedio;
+		double ticketMedio, resultTicket;
+		
+		final double VLRCOMPRA = 3.000;
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE ");
 		System.out.println("--------------------------------");
-		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println("INFORME OS DADOS DO ÃšLTIMO ANO");
 		
-		System.out.printf("Quantas compras o cliente fez no último ano? ");
+		System.out.printf("Quantas compras o cliente fez no Ãºltimo ano? ");
 		qtdCompras = sc.nextInt();
 		
-		System.out.printf("Qual o ticket médio? ");
+		System.out.printf("Qual o ticket mÃ©dio? ");
 		ticketMedio = sc.nextDouble();
 		
 		System.out.printf("Quantas vezes o cliente atrasou o pagamento? ");
 		qtdAtrasoCliente = sc.nextInt();
 		
-		System.out.printf("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		System.out.printf("A maioria das compras foi em dinheiro, cartÃ£o, ou boleto (D/C/B)? ");
 		tipoPagamento = sc.next().charAt(0);
+		
+		
+		resultTicket = qtdCompras * ticketMedio;
+		if (qtdCompras <= 0) {
+			System.out.printf("Score de volume de compras = 0 pontos");
+		}
+		else if(resultTicket <= VLRCOMPRA && qtdCompras <= 2) {
+			System.out.printf("Score de volume de compras = 20 pontos");
+		}else if(resultTicket <= VLRCOMPRA  && qtdCompras > 2) {
+			System.out.printf("Score de volume de compras = 40 pontos");
+		}
+		else {
+			System.out.printf("Score de volume de compras = 60 pontos");
+		}
 	}
 
 }
